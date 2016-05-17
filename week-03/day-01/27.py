816d7ef9e06895855356398a72a8c67472c39b15
bizkanta <totivan.aliz@gmail.com>
2016-05-17 17:22:09 +0200

    Solved 21-31 py excercises

---
+++ week-03/day-01/27.py
@@ -0,0 +1,10 @@
+z = 13
+# if z is between 10 and 20 print
+# if less than 10 print 'More!',
+# if more than 20 print 'Less!'
+if z > 20:
+  print('Less!')
+elif z > 10:
+  print('Sweet!')
+else:
+  print('More')
---
+++ week-03/day-01/31.py
@@ -0,0 +1,5 @@
+ad = 6
+# print the numbers till ad from 0
+while ad > 0:
+  ad -= 1
+  print(ad)
