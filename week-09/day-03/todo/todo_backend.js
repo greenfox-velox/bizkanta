dfa7cb09fbc7fd923bbbc4fab4e99236d5edbc65
bizkanta <totivan.aliz@gmail.com>
2016-07-13 20:12:13 +0200

    added todo app with connected database

---
+++ week-09/day-03/todo/todo_backend.js
@@ -0,0 +1,104 @@
+'use strict';
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var mysql = require("mysql");
+var app = express();
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password987",
+  database: "todo"
+});
+
+con.connect(function(err){
+  if(err){
+    console.log("Error connecting to Db");
+    return;
+  }
+  console.log("Connection established");
+});
+
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.use(bodyParser.json());
+app.use(express.static('./'));
+
+// var todos = [
+//     {
+//         "completed": false,
+//         "id": 1,
+//         "text": "Buy milk"
+//     },
+//     {
+//         "completed": false,
+//         "id": 2,
+//         "text": "Make dinner"
+//     },
+//     {
+//         "completed": false,
+//         "id": 3,
+//         "text": "Save the world"
+//     }
+// ];
+
+app.get('/todos/', function(req, res) {
+  con.query('SELECT * FROM todos;',function(err,rows){
+    if(err) {
+      console.log(err.toString());
+      return;
+    }
+    rows.map(function(item) {
+      item.completed = (item.completed === 1);
+    });
+    res.send(rows);
+  });
+});
+
+app.get('/todos/:id', function(req, res) {
+  con.query('SELECT * FROM todos WHERE id = ' + req.params.id,function(err,todo){
+    if(err) {
+      console.log(err.toString());
+      return;
+    }
+    res.send(todo[0]);
+  });
+});
+
+app.post('/todos/', urlencodedParser, function(req, res) {
+  con.query("INSERT INTO todos (text) VALUES ('" + req.body.text + "')" ,function(err,todo){
+    if(err) {
+      console.log(err.toString());
+      return;
+    }
+    console.log(todo);
+    res.send({id: todo.insertId, text: req.body.text});
+  });
+});
+
+app.put('/todos/:id', urlencodedParser, function(req, res) {
+  var completedStatusInDb = req.body.completed ? 1 : 0;
+  con.query("UPDATE todos SET completed = ? WHERE id = ?", [completedStatusInDb, req.params.id], function(err,todo){
+    if(err) {
+      console.log(err.toString());
+      return;
+    }
+    res.send({id: req.params.id, completed: req.body.completed});
+  });
+});
+
+app.delete('/todos/:id', urlencodedParser, function(req, res) {
+  con.query("DELETE FROM todos WHERE id = ?", req.params.id,function(err,todo){
+    if(err) {
+      console.log(err.toString());
+      return;
+    }
+    res.send({id: req.params.id, destroyed: true});
+  });
+});
+
+var port = 3000;
+app.listen(port, function(){
+  console.log('I am listening on port ' + port);
+});
